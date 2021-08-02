88c397baece234264d5a3d7ee1f9fbe62a0d3607
Sonam <sonam.malhotra2496@gmail.com>
2021-08-02 22:13:11 +0000

    Accessed URL

--- express_server.js
+++ express_server.js
@@ -14,7 +14,8 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: "URL"
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] 
+
    };
   res.render("urls_show", templateVars);
 });
