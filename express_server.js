e73d90d6a6039e012a996aabe465aabd43a3f449
Sonam <sonam.malhotra2496@gmail.com>
2021-08-03 21:04:35 +0000

    tiny app

--- express_server.js
+++ express_server.js
@@ -58,9 +58,7 @@ app.post ("/urls/:shortURL/delete",(req,res) => {
 
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] 
-
-   };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
