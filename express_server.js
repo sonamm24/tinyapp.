468f8915d77f8dc9d38078a1fdcbad64d45b23a4
Sonam <sonam.malhotra2496@gmail.com>
2021-08-03 17:40:20 +0000

    intalled Nodeman

--- express_server.js
+++ express_server.js
@@ -9,16 +9,14 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] 
-
-   };
-  res.render("urls_show", templateVars);
-});
+// app.get("/urls/:id", ...
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -44,7 +42,48 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send(generateRandomString());         // Respond with 'Ok' (we will replace this)
+});
+
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] 
+
+   };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+function generateRandomInteger(min,max) {
+  return Math.floor(Math.random() * (max-min+1)) + min;
+}
+
+function generateRandomString() {
+  let result = "";
+  
+  for(let i = 0; i<6 ; i++){
+    let choice = generateRandomInteger(1,3);
+    let randNum = 0;
+    if(choice === 1) {
+      randNum = generateRandomInteger(97,122);
+    } else if (choice === 2) {
+      randNum = generateRandomInteger(65,90);
+    } else {
+      randNum = generateRandomInteger(48,57);
+    }
+
+    let character = String.fromCharCode(randNum);
+    result+=character;
+  }
+  console.log(result);
+  return result;
+}
